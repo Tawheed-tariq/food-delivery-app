567261b0c6d9e0290370df2c9f8ed3e3c1d4b352
Tawheed-Tariq <tavaheed_2022bite008@nitsri.ac.in>
2024-06-06 20:13:06 +0530

    farmer motion 2

--- src/pages/CuisinePage.js
+++ src/pages/CuisinePage.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import FoodItem from '../components/FoodItem';
 import { foodItems } from '../data/foodData';
+import { motion } from 'framer-motion';
 
 const CuisinePage = ({ cuisine, addToCart }) => {
   const cuisineItems = foodItems.filter(item => item.cuisine === cuisine);
 
   return (
     <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-6 text-center text-gray-800 capitalize">{cuisine} Cuisine</h1>
+      <motion.h1 
+        className="text-3xl font-bold mb-6 text-center text-gray-800 capitalize"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+          {cuisine} Cuisine
+      </motion.h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {cuisineItems.map(item => (
-          <FoodItem key={item.id} item={item} onAddToCart={addToCart} />
+        {cuisineItems.map((item, index) => (
+          <motion.div
+          key={item.id}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: index * 0.1 }}
+        >
+          <FoodItem  item={item} onAddToCart={addToCart} />
+        </motion.div>
         ))}
       </div>
     </div>
